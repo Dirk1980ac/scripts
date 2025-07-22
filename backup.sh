839c40f6a3ab33d5eadf02b2fa6e4b803fb24149
Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
2025-07-22 21:37:08 +0200

    Improve scripts.

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 fi
 
 # Export GPG passphrase for duplicity to use
-export SIGN_PASSPHRASE=$1
+export SIGN_PASSPHRASE="$1"
 
 # Run Duplicity with the specified options and parameters
 duplicity \
@@ -18,25 +18,25 @@ duplicity \
 	--sign-key 82C39B0F4FED68CDFF63F7000EA6FF1FE712FE10 \
 	--encrypt-key 82C39B0F4FED68CDFF63F7000EA6FF1FE712FE10 \
 	--encrypt-key A0A9E62144A0D5030E68E99D75A628B3D4D5B768 \
-	--include $HOME/Dokumente \
-	--include $HOME/Bilder \
-	--include $HOME/Musik \
-	--include $HOME/.config \
-	--include $HOME/.gnupg \
-	--include $HOME/Vorlagen \
-	--include $HOME/bin \
-	--include $HOME/.ssh \
-	--include $HOME/.zshrc \
-	--include $HOME/.kodi \
-	--include $HOME/.password-store \
-	--include $HOME/.antigenrc \
-	--include $HOME/.zshrc \
-	--include $HOME/.antigen \
-	--include $HOME/scripts \
-	--include $HOME/rpmbuild/SPECS \
-	--include $HOME/rpmbuild/SOURCES \
+	--include "$HOME"/Dokumente \
+	--include "$HOME"/Bilder \
+	--include "$HOME"/Musik \
+	--include "$HOME"/.config \
+	--include "$HOME"/.gnupg \
+	--include "$HOME"/Vorlagen \
+	--include "$HOME"/bin \
+	--include "$HOME"/.ssh \
+	--include "$HOME"/.zshrc \
+	--include "$HOME"/.kodi \
+	--include "$HOME"/.password-store \
+	--include "$HOME"/.antigenrc \
+	--include "$HOME"/.zshrc \
+	--include "$HOME"/.antigen \
+	--include "$HOME"/scripts \
+	--include "$HOME"/rpmbuild/SPECS \
+	--include "$HOME"/rpmbuild/SOURCES \
 	--exclude '**' \
-	$HOME \
+	"$HOME" \
 	file:///net/lexington/dgottschalk/backup/reliant
 
 # Unset the GPG passphrase after the backup is complete
